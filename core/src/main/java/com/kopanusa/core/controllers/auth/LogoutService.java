0915e300223faafd209bf549b29f04289523f49b
Mochammad Rizki Romdoni <mr.romdoni@gmail.com>
2024-04-27 10:46:53 +0700

    logout service

---
+++ core/src/main/java/com/kopanusa/core/controllers/auth/LogoutService.java
@@ -0,0 +1,41 @@
+package com.kopanusa.core.controllers.auth;
+
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.web.authentication.logout.LogoutHandler;
+import org.springframework.stereotype.Service;
+import org.springframework.security.core.Authentication;
+
+import com.kopanusa.core.repositories.TokenRepository;
+
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import lombok.RequiredArgsConstructor;
+
+@Service
+@RequiredArgsConstructor
+public class LogoutService implements LogoutHandler
+{
+  private final TokenRepository tokenRepository;
+
+  @Override
+  public void logout(HttpServletRequest request, HttpServletResponse response, Authentication authentication) 
+  {
+    final String authHeader = request.getHeader("Authorization");
+    final String jwt;
+
+    if (authHeader == null || !authHeader.startsWith("Bearer "))
+    {
+      return;
+    }
+    jwt = authHeader.substring(7);
+    var storedToken = tokenRepository.findByToken(jwt).orElse(null);
+    if (storedToken != null) 
+    {
+      storedToken.setExpired(true);
+      storedToken.setRevoked(true);
+      tokenRepository.save(storedToken);
+      SecurityContextHolder.clearContext();
+    }
+  }
+}
